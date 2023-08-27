941761ed99ce909063d968c000f3d8952df2e835
MarcinSz1993 <marcinsz1993@hotmail.com>
2023-08-27 10:50:45 +0200

    Initial commit

---
+++ src/main/java/com/example/roomreservations/RoomreservationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.roomreservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomreservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomreservationsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/roomreservations/model/Reservation.java
@@ -0,0 +1,2 @@
+package com.example.roomreservations.model;public class Reservation {
+}
