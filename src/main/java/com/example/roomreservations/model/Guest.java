b26cb24b7f52a3c901f5021ab6f342d956355e05
MarcinSz1993 <marcinsz1993@hotmail.com>
2023-08-27 12:36:05 +0200

    Add class 'Guest'.

---
+++ src/main/java/com/example/roomreservations/model/Guest.java
@@ -0,0 +1,30 @@
+package com.example.roomreservations.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity(name = "guest")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Guest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+    @Column(name = "email_address")
+    private String email;
+    @Column(name = "phoneNumber")
+    private String phoneNumber;
+}
